cae6cfd8bb406ee561222c09b0864105b08880d1
a.y <g.slackware@gmail.com>
2025-10-14 08:25:41 +0000

    vite.config修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // true: devcontainerを使用するため、すべてのネットワークインターフェースでリッスン（0.0.0.0）
+    port: 5173, // Viteのデフォルトポート
+    strictPort: true, //ポート固定
+    watch: {
+      usePolling: true,  // ← ファイル変更検知
+    },
+  },
 })
