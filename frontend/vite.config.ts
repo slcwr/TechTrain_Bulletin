3df9937e80fed657136d5efa5ac2fb9481d90a21
a.y <g.slackware@gmail.com>
2025-10-14 11:01:08 +0000

    フォルダ構成

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import Pages from 'vite-plugin-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  Pages({
+      // ページディレクトリ
+      dirs: 'src/pages',
+
+      // 拡張子
+      extensions: ['tsx', 'ts'],
+
+      // 除外するファイル
+      exclude: ['**/components/**', '**/*.test.tsx'],
+    }),
+    ],
   server: {
     host: true, // true: devcontainerを使用するため、すべてのネットワークインターフェースでリッスン（0.0.0.0）
     port: 5173, // Viteのデフォルトポート
